3c6b9d0b502d1f1f4c71bc98a87d7db13f81cd9f
Sebastian Wiesner <sebastian@swsnr.de>
2020-06-04 20:14:32 +0200

    Persist and validate downloads

--- src/home.rs
+++ src/home.rs
@@ -4,31 +4,31 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::io::Write;
+use std::os::unix::ffi::OsStrExt;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Stdio};
 
-use anyhow::{anyhow, Context, Error, Result};
 use colored::*;
 use fehler::{throw, throws};
 use semver::Version;
-
-use crate::Manifest;
 use url::Url;
+
+use anyhow::{anyhow, Context, Error, Result};
+
+use crate::{Checksums, Manifest};
 
 pub struct Home {
     home: PathBuf,
+    cache_dir: PathBuf,
 }
 
 #[throws]
 fn curl(url: &Url, target: &Path) -> () {
     let mut child = Command::new("curl")
-        .arg("--disable")
-        .arg("--globoff")
-        .arg("--cookie")
+        .arg("-gqb")
         .arg("")
-        .arg("--fail")
-        .arg("--location")
-        .arg("--continue-at")
+        .arg("-fLC")
         .arg("-")
         .arg("--progress-bar")
         .arg("--retry")
@@ -62,11 +62,95 @@ fn curl(url: &Url, target: &Path) -> () {
     }
 }
 
+#[throws]
+fn validate(target: &Path, checksums: &Checksums) -> () {
+    let mut child = Command::new("b2sum")
+        .arg("-c")
+        .stdin(Stdio::piped())
+        .spawn()
+        .with_context(|| format!("Failed to spawn b2sum for {}", target.display()))?;
+
+    let mut stdin = child.stdin.take().expect("Stdin was piped?");
+    write!(stdin, "{} ", checksums.b2)
+        .and_then(|_| stdin.write_all(target.as_os_str().as_bytes()))
+        .with_context(|| format!("Failed to pipe checksum to b2sum for {}", target.display()))?;
+    drop(stdin);
+
+    let status = child
+        .wait()
+        .with_context(|| format!("Failed to wait for b2sum for {}", target.display()))?;
+
+    if !status.success() {
+        throw!(anyhow!("b2sum failed to validate {}", target.display()));
+    }
+}
+
+#[throws]
+fn maybe_extract(directory: &Path, file: &Path) -> () {
+    let filename = file.file_name().unwrap_or_default().to_string_lossy();
+    if filename.ends_with(".tar.gz")
+        || filename.ends_with(".tar.bz2")
+        || filename.ends_with(".tar.xz")
+    {
+        let status = Command::new("tar")
+            .arg("xf")
+            .arg(file)
+            .arg("-C")
+            .arg(directory)
+            .spawn()
+            .with_context(|| {
+                format!(
+                    "Failed to spawn tar xf {} -C {}",
+                    file.display(),
+                    directory.display()
+                )
+            })?
+            .wait()
+            .with_context(|| {
+                format!(
+                    "Failed to wait for spawn tar xf {} -C {}",
+                    file.display(),
+                    directory.display()
+                )
+            })?;
+
+        if !status.success() {
+            throw!(anyhow!(
+                "tar xf {} -C {} failed with exit code {}",
+                file.display(),
+                directory.display(),
+                status,
+            ))
+        }
+    } else {
+        ()
+    }
+}
+
 impl Home {
-    pub fn new() -> Result<Home> {
-        dirs::home_dir()
-            .ok_or_else(|| anyhow!("Home directory does not exist"))
-            .map(|home| Home { home })
+    #[throws]
+    pub fn new() -> Home {
+        let home = dirs::home_dir().ok_or_else(|| anyhow!("Home directory does not exist"))?;
+        let cache_dir = dirs::cache_dir()
+            .map(|d| d.join("homebins"))
+            .ok_or_else(|| anyhow!("Cache directory not available"))?;
+
+        let home = Home { home, cache_dir };
+        home.ensure_dirs()?;
+        home
+    }
+
+    fn ensure_dirs(&self) -> Result<()> {
+        std::fs::create_dir_all(&self.download_dir()).with_context(|| {
+            format!(
+                "Failed to create download directory at {}",
+                self.download_dir().display()
+            )
+        })
+    }
+
+    pub fn download_dir(&self) -> PathBuf {
+        self.cache_dir.join("downloads")
     }
 
     pub fn bin_dir(&self) -> PathBuf {
@@ -144,11 +228,14 @@ impl Home {
         })?;
 
         for install in &manifest.install {
-            println!("Downloading {}", install.download.as_str().bold());
-            curl(
-                &install.download,
-                &work_dir.path().join(install.filename()?),
-            )?;
+            let target = self.download_dir().join(install.filename()?);
+            if !target.is_file() {
+                println!("Downloading {}", install.download.as_str().bold());
+                curl(&install.download, &target)?;
+            }
+            validate(&target, &install.checksums)?;
+            maybe_extract(work_dir.path(), &target)?;
+            // install_files(&install.files)?
         }
         Command::new("ls")
             .arg("-la")
